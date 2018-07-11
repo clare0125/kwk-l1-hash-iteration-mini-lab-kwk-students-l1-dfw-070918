452378c55d05c472b2c7fa2768be3b86bafa6e7c
clare0125 <clare.woozie1@gmail.com>
2018-07-11 19:41:53 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -55,3 +55,5 @@ summer_olympics_hash.keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
 end
+
+iterate_through_keys

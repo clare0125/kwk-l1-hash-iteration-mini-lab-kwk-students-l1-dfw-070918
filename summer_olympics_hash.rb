57c3723f3eb02ecddf0b7aad2d63aff75f5d23af
clare0125 <clare.woozie1@gmail.com>
2018-07-11 20:25:07 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,39 +1,16 @@
 
 def create_olympics_hash
-  
-  # place_array = ["Sydney", "Athens", "Beijing", "London"]
-  # years = [2000, 2004, 2008, 2012]
-  # summer_olympics_hash = {}
-  # years_number = 0
-  # years_number += 1
-  # place_array.each do |each_place|
-  #   summer_olympics_hash[each_place] = years_number
-    
 summer_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
-  # Implement this method so that it returns a hash with the data provided on README.md
 end
 
 puts create_olympics_hash
 
 
-
-
-
-
 def add_a_key_value_pair
-  
 summer_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
   summer_olympics_hash[:Atlanta] = "1996"
   return summer_olympics_hash
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
-
-add_a_key_value_pair
-
-
-
-
-
 
 def iterate_through_hash
 summer_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012", :Atlanta => "1996"}
@@ -43,12 +20,6 @@ summer_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008"
 end
 
 def iterate_through_keys
-
 upcased_cities = add_a_key_value_pair.map{|key, value| key.upcase}
-
 upcased_cities.each{|city| puts city}
-
-    
 end
-
-

29d9dd2695496dab57ae0afee5204f7b470e72b7
clare0125 <clare.woozie1@gmail.com>
2018-07-11 20:03:38 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,19 +1,19 @@
 
-# def create_olympics_hash
+def create_olympics_hash
   
-#   # place_array = ["Sydney", "Athens", "Beijing", "London"]
-#   # years = [2000, 2004, 2008, 2012]
-#   # summer_olympics_hash = {}
-#   # years_number = 0
-#   # years_number += 1
-#   # place_array.each do |each_place|
-#   #   summer_olympics_hash[each_place] = years_number
+  # place_array = ["Sydney", "Athens", "Beijing", "London"]
+  # years = [2000, 2004, 2008, 2012]
+  # summer_olympics_hash = {}
+  # years_number = 0
+  # years_number += 1
+  # place_array.each do |each_place|
+  #   summer_olympics_hash[each_place] = years_number
     
-# summer_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
-#   # Implement this method so that it returns a hash with the data provided on README.md
-# end
+summer_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
+  # Implement this method so that it returns a hash with the data provided on README.md
+end
 
-# puts create_olympics_hash
+puts create_olympics_hash
 
 
 
